cb6cb12f5e7824e285f4e2b0bebd6c4de3ba73a9
Romulo <romulosilva001@gmail.com>
2022-02-11 11:04:55 -0300

    Alterado botão play e pause para ter apenas um

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let audio = document.getElementById("audio");
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+
 
 function retroceder() {
   audio.currentTime -= 15; 
@@ -10,8 +13,12 @@ function avancar() {
 
 function iniciar() {
   audio.play();
+  play.style.display = "none";
+  pause.style.display = "inline";
 }
 
 function pausar() {
   audio.pause();
+  pause.style.display = "none";
+  play.style.display = "inline";
 }
